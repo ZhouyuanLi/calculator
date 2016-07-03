b46bf5300f9e4c3f427e6c15c57cb5e2369759ff
Zhouyuan Li <zhouyuanl@zhouyuanl-macbookpro.roam.corp.google.com>
2016-07-02 18:38:42 -0700

    auto layout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -24,8 +24,40 @@ class ViewController: UIViewController {
     }
     
     @IBAction func operate(sender: UIButton) {
-        
+        if userIsInTheMiddleOfTypingNumber {
+            enter()
+        }
+        let operation = sender.currentTitle!
+        switch operation {
+            case "+":
+                performOperation({(op1, op2) in op1 + op2})
+            case "-":
+                performOperation({(op1, op2) in op2 - op1})
+            case "✕":
+                performOperation({(op1, op2) in op1 * op2})
+            case "÷":
+                performOperation({(op1, op2) in op2 / op1})
+            case "√":
+                performOperation2({op1 in sqrt(op1)})
+            default:
+                break
+        }
     }
+    
+    func performOperation(operation : (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation2(operation : Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
     var operandStack = Array<Double>()
     
     @IBAction func enter() {
